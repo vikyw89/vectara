5e2aa5b492c867ea826a5fb6c6545c5414eb1e16
vikyw89 <vikyw89@gmail.com>
2023-11-04 17:15:18 +0800

    feat(prisma): add Prisma schema file to define data models and relationships feat(pyproject.toml): add Prisma as a dependency to the project feat(src/libs/agent/index.py): add classes for Request, Step, Memory, and Agent to handle stateful and stateless agents

--- src/libs/agent/index.py
+++ src/libs/agent/index.py
@@ -1,4 +1,59 @@
+from pydantic import BaseModel, Field
+
+
+class Request(BaseModel):
+    id: str
+    agentId: str
+    userId: str
+    history: list
+    data: any = Field(default=None)
+    type: str = Field(default="unknown")
+
+
+class Step:
+    def __init__(self, name, description, stepFn) -> None:
+        self.name = name
+        self.description = description
+        self.stepFn = stepFn
+
+    def run(self, request):
+        return self.stepFn(request)
+
+# This class is to handle any stateful data
+
+
+class Memory:
+    def __init__(self, name, description, saveFn, recallFn, logFn) -> None:
+        self.name = name
+        self.description = description
+        self.saveFn = saveFn
+        self.recallFn = recallFn
+        self.logFn = logFn
+
+    def log(self, request):
+        return self.logFn(request)
+
+    def save(self, request):
+        self.logFn(request)
+        return self.saveFn(request)
+
+    def recall(self, request):
+        self.logFn(request)
+        return self.recallFn(request)
+
+# stateless agents, designed to be customizable, 1 memory module and endless steps
+
+
 class Agent:
-    def __init__(self) -> None:
-        
-        
+    def __init__(self, memory, steps: list(Step)) -> None:
+        self.steps = steps
+        self.memory = memory
+
+    def run(self, request):
+        steps = self.steps
+        self.memory.save(request)
+
+        for step in steps:
+            output = step.run(request)
+            self.memory.save(output)
+        return memory
